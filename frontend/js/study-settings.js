4b29eb5c8d011c609777aadf017ecee827413999
Wilson Junior <wilson.junior@mxdrive.com.br>
2026-01-30 17:03:51 -0300

    chore/styles: lógica para renderização das matérias por áreas de conhecimento

--- frontend/js/study-settings.js
+++ frontend/js/study-settings.js
@@ -1,26 +1,73 @@
 const STUDY_SETTING_SECTION_DOM = {
   areaItens: document.querySelectorAll(".item-area"),
+  subjectsList: document.querySelector(".study-subject-list"),
 };
 
-const SetArea = (event) => {
-  const li = event.target;
-  if (!li) return;
+let payload = {
+  area: "",
+  subject: "",
+  time: "",
+  type: "",
+};
 
-  const classItem = li.dataset.class_item;
+const subjectsByAreas = {
+  langs: [
+    "Português",
+    "Literatura",
+    "Inglês",
+    "Espanhol",
+    "Artes",
+    "Educação Física",
+  ],
+  human_sciences: [
+    "História",
+    "Geografia",
+    "Filosofia",
+    "Filosofia",
+    "Sociologia",
+  ],
+  natural_sciences: ["Física", "Química", "Matemática"],
+  exacts: ["Álgebra", "Geometria", "Estatística", "Probabilidade", "Funções"],
+};
 
-  // remove classes de todos
+const bindSubjects = (area) => {
+  STUDY_SETTING_SECTION_DOM.subjectsList.innerHTML = "";
+  area.forEach((subject) => {
+    STUDY_SETTING_SECTION_DOM.subjectsList.innerHTML += `
+    <li class="study-subject-list-item general-list-item">
+        <span>${subject}</span>
+    </li>`;
+  });
+  console.log(area);
+};
+
+const setArea = (event) => {
+  const areaEl = event.target;
+  if (!areaEl) return;
+  const area = areaEl.closest("li").dataset.area;
+
+  payload.area = area;
+
+  bindSubjects(subjectsByAreas[area]);
+
+  const classItem = areaEl.dataset.class_item;
+
   STUDY_SETTING_SECTION_DOM.areaItens.forEach((item) => {
-    item.closest("li").classList.remove(
-      "purple-item",
-      "yellow-item",
-      "green-item",
-      "blue-item",
-    );
+    item
+      .closest("li")
+      .classList.remove(
+        "purple-item",
+        "yellow-item",
+        "green-item",
+        "blue-item",
+      );
   });
 
-  li.closest("li").classList.add(classItem);
+  areaEl.closest("li").classList.add(classItem);
 };
 
+const setSubject = () => {};
+
 STUDY_SETTING_SECTION_DOM.areaItens.forEach((item) => {
-  item.addEventListener("click", SetArea);
+  item.addEventListener("click", setArea);
 });
