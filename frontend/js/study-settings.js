09c577d598ba1c123e022656f5bb80a7061ee0ed
willsf2021 <willsf2021@gmail.com>
2026-01-31 15:05:22 -0300

    styles: lógica para selecionar o tempo e a matéria

--- frontend/js/study-settings.js
+++ frontend/js/study-settings.js
@@ -1,6 +1,9 @@
 const STUDY_SETTING_SECTION_DOM = {
   areaItens: document.querySelectorAll(".item-area"),
+  timeItens: document.querySelectorAll(".item-time"),
+  subjectsItens: null,
   subjectsList: document.querySelector(".study-subject-list"),
+  subjectsSection: document.querySelector(".study-subject"),
 };
 
 let payload = {
@@ -11,34 +14,52 @@ let payload = {
 };
 
 const subjectsByAreas = {
-  langs: [
-    "Português",
-    "Literatura",
-    "Inglês",
-    "Espanhol",
-    "Artes",
-    "Educação Física",
-  ],
   human_sciences: [
-    "História",
-    "Geografia",
-    "Filosofia",
-    "Filosofia",
-    "Sociologia",
+    ["História", "history"],
+    ["Geografia", "geography"],
+    ["Filosofia", "philosophy"],
+    ["Sociologia", "sociology"],
   ],
-  natural_sciences: ["Física", "Química", "Matemática"],
-  exacts: ["Álgebra", "Geometria", "Estatística", "Probabilidade", "Funções"],
+
+  natural_sciences: [
+    ["Física", "physics"],
+    ["Química", "chemistry"],
+    ["Matemática", "mathematics"],
+  ],
+
+  exacts: [
+    ["Álgebra", "algebra"],
+    ["Geometria", "geometry"],
+    ["Estatística", "statistics"],
+    ["Probabilidade", "probability"],
+    ["Funções", "functions"],
+  ],
 };
 
 const bindSubjects = (area) => {
   STUDY_SETTING_SECTION_DOM.subjectsList.innerHTML = "";
+
+  // ativa animação
+  STUDY_SETTING_SECTION_DOM.subjectsSection.classList.add("show");
+
   area.forEach((subject) => {
-    STUDY_SETTING_SECTION_DOM.subjectsList.innerHTML += `
-    <li class="study-subject-list-item general-list-item">
-        <span>${subject}</span>
-    </li>`;
+    STUDY_SETTING_SECTION_DOM.subjectsList.insertAdjacentHTML(
+      "beforeend",
+      `
+      <li class="study-subject-list-item general-list-item" data-subject="${subject[1]}">
+        <div class="item-subject"></div>
+        <span>${subject[0]}</span>
+      </li>
+      `,
+    );
   });
-  console.log(area);
+
+  STUDY_SETTING_SECTION_DOM.subjectsItens =
+    document.querySelectorAll(".item-subject");
+
+  STUDY_SETTING_SECTION_DOM.subjectsItens.forEach((item) => {
+    item.addEventListener("click", setSubject);
+  });
 };
 
 const setArea = (event) => {
@@ -47,6 +68,7 @@ const setArea = (event) => {
   const area = areaEl.closest("li").dataset.area;
 
   payload.area = area;
+  payload.subject = "";
 
   bindSubjects(subjectsByAreas[area]);
 
@@ -66,8 +88,43 @@ const setArea = (event) => {
   areaEl.closest("li").classList.add(classItem);
 };
 
-const setSubject = () => {};
+const setSubject = (event) => {
+  const subjectEl = event.target;
+  if (!subjectEl) return;
+  const subject = subjectEl.closest("li").dataset.subject;
+
+  payload.subject = subject;
+
+  const classItem = "subject-active";
+  // console.log(STUDY_SETTING_SECTION_DOM.subjectsItens);
+  STUDY_SETTING_SECTION_DOM.subjectsItens.forEach((item) => {
+    item.closest("li").classList.remove(classItem);
+  });
+
+  subjectEl.closest("li").classList.add(classItem);
+  console.log(payload);
+};
+
+const setTime = (event) => {
+  const timeEl = event.target;
+  if (!timeEl) return;
+  const time = timeEl.closest("li").dataset.time;
+
+  payload.time = time;
+
+  const classItem = "time-active";
+
+  STUDY_SETTING_SECTION_DOM.timeItens.forEach((item) => {
+    item.closest("li").classList.remove(classItem);
+  });
+
+  timeEl.closest("li").classList.add(classItem);
+  console.log(payload);
+};
 
 STUDY_SETTING_SECTION_DOM.areaItens.forEach((item) => {
   item.addEventListener("click", setArea);
+});
+STUDY_SETTING_SECTION_DOM.timeItens.forEach((item) => {
+  item.addEventListener("click", setTime);
 });
